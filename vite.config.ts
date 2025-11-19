5e9fd3d8e7718a41c88b4369cbc2f8bc0b71b606
t0mm0 <dev@onairsoftware.co.uk>
2025-11-19 00:35:48 +0000

    fix routing "properly"
    copy index.html to a file for each route as github pages can't deal with wildcards

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,51 @@ export default defineConfig({
           src: path.resolve(import.meta.dirname, 'public/audio/*.mp3'),
           dest: 'audio',
         },
+        {
+          src: path.resolve(import.meta.dirname, 'public/og-image.svg'),
+          dest: '',
+        },
+        // bodge routing as github can't handle wildcards
+        {
+          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+          dest: '',
+          rename: 'about.html',
+        },
+        {
+          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+          dest: '',
+          rename: 'instagram.html',
+        },
+        {
+          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+          dest: '',
+          rename: 'theory.html',
+        },
+        {
+          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+          dest: '',
+          rename: 'instruments.html',
+        },
+        {
+          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+          dest: '',
+          rename: 'protest-beats.html',
+        },
+        {
+          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+          dest: '',
+          rename: 'tunes.html',
+        },
+        {
+          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+          dest: '',
+          rename: 'experiment.html',
+        },
+        {
+          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+          dest: '',
+          rename: 'shop.html',
+        },
       ],
     }),
   ],
