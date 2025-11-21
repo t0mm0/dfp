6e1f612488901d589fd3f8e485d4e41c9d46b06b
t0mm0 <dev@onairsoftware.co.uk>
2025-11-21 21:29:01 +0000

    fix dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -11,64 +11,61 @@ export default defineConfig({
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
+          viteStaticCopy({
+            targets: [
+              {
+                src: path.resolve(import.meta.dirname, 'public/audio/*.mp3'),
+                dest: 'audio',
+              },
+              {
+                src: path.resolve(import.meta.dirname, 'public/og-image.svg'),
+                dest: '',
+              },
+              // bodge routing as github can't handle wildcards
+              {
+                src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+                dest: '',
+                rename: 'about.html',
+              },
+              {
+                src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+                dest: '',
+                rename: 'instagram.html',
+              },
+              {
+                src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+                dest: '',
+                rename: 'theory.html',
+              },
+              {
+                src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+                dest: '',
+                rename: 'instruments.html',
+              },
+              {
+                src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+                dest: '',
+                rename: 'protest-beats.html',
+              },
+              {
+                src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+                dest: '',
+                rename: 'tunes.html',
+              },
+              {
+                src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+                dest: '',
+                rename: 'experiment.html',
+              },
+              {
+                src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
+                dest: '',
+                rename: 'shop.html',
+              },
+            ],
+          }),
         ]
       : []),
-    viteStaticCopy({
-      targets: [
-        {
-          src: path.resolve(import.meta.dirname, 'public/audio/*.mp3'),
-          dest: 'audio',
-        },
-        {
-          src: path.resolve(import.meta.dirname, 'public/og-image.svg'),
-          dest: '',
-        },
-        // bodge routing as github can't handle wildcards
-        {
-          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
-          dest: '',
-          rename: 'about.html',
-        },
-        {
-          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
-          dest: '',
-          rename: 'instagram.html',
-        },
-        {
-          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
-          dest: '',
-          rename: 'theory.html',
-        },
-        {
-          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
-          dest: '',
-          rename: 'instruments.html',
-        },
-        {
-          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
-          dest: '',
-          rename: 'protest-beats.html',
-        },
-        {
-          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
-          dest: '',
-          rename: 'tunes.html',
-        },
-        {
-          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
-          dest: '',
-          rename: 'experiment.html',
-        },
-        {
-          src: path.resolve(import.meta.dirname, 'dist/public/index.html'),
-          dest: '',
-          rename: 'shop.html',
-        },
-      ],
-    }),
   ],
   resolve: {
     alias: {
